a130f83f82bb25fc350512354d2c93bf8bad9666
Guest User <Guest@ajays-macbook-pro.local>
2016-03-17 21:30:06 +0530

    Initial Commit

---
+++ PropKaro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PropKaro
+//
+//  Created by Guest User on 3/17/16.
+//  Copyright © 2016 Ajay Awasthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PropKaro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PropKaro
+//
+//  Created by Guest User on 3/17/16.
+//  Copyright © 2016 Ajay Awasthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
